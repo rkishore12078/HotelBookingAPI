2d6e9c42517a0521cf178f0de8c32cefb04967cc
Kishore <kishore.kanini@gmail.com>
2023-05-26 18:42:47 +0530

    hotel api updated

---
+++ HotelBookingApplication/HotelAPI/Models/DTO/PriceDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelAPI.Models.DTO
+{
+    public class PriceDTO
+    {
+        public int H_Id { get; set; }
+        public double MinValue { get; set; }
+        public double MazValue { get; set; }
+    }
+}
