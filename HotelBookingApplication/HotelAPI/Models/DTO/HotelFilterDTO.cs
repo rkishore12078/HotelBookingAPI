90bc2b08b8071de7ef12ad9ef35d528ad46b8015
Kishore <kishore.kanini@gmail.com>
2023-05-26 18:42:33 +0530

    hotel api updated

---
+++ HotelBookingApplication/HotelAPI/Models/DTO/HotelFilterDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelAPI.Models.DTO
+{
+    public class HotelFilterDTO
+    {
+        public string? city { get; set; }
+        public string? country { get; set; }
+        public string? Amenities { get; set; }
+    }
+}
