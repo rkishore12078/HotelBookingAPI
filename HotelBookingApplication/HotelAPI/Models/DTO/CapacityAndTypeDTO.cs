89bb2962c3c49ebfb47998be86c2a9709c4272b9
Kishore <kishore.kanini@gmail.com>
2023-05-26 18:41:52 +0530

    hotel api updated

---
+++ HotelBookingApplication/HotelAPI/Models/DTO/CapacityAndTypeDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelAPI.Models.DTO
+{
+    public class CapacityAndTypeDTO
+    {
+        public int H_id { get; set; }
+        public int? Capacity { get; set; }
+        public string? Type { get; set; }
+    }
+}
